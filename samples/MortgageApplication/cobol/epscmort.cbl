ec0e44d8782e686f74a530f38dd12ce7ee817375
drakshit <drakshit@LIN-5CG1349JL0.corp.capgemini.com>
2024-10-17 19:33:15 +0530

    commit 19.33

--- samples/MortgageApplication/cobol/epscmort.cbl
+++ samples/MortgageApplication/cobol/epscmort.cbl
@@ -54,7 +54,7 @@
        01 IBMREQD                           PIC X(1).
       *
        01  END-OF-TRANS-MSG                 PIC X(30)
-             VALUE 'END OF TRAN- thanks!'.
+             VALUE 'END OF TRAN- thank you!'.
        01  BLANK-MSG                        PIC X(1) VALUE ' '.
            COPY DFHAID.
       *    COPY DFHEIBLK.
